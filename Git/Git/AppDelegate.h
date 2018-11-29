0d146174113f840e43991a7bd28882b057436b51
thb <tianhongbo@tianhongbodeMacBook-Pro.local>
2018-11-29 15:00:48 +0800

    First Commit

---
+++ Git/Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by 田红波 on 2018/11/29.
+//  Copyright © 2018年 田红波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git/Git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git
+//
+//  Created by 田红波 on 2018/11/29.
+//  Copyright © 2018年 田红波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
